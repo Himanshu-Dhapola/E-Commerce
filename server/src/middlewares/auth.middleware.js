7b9da997214e84c976f2379362f3e1c741198d53
Himanshu Dhapola <143524162+Himanshu-Dhapola@users.noreply.github.com>
2024-09-06 16:35:51 +0530

    Update auth.middleware.js

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -3,8 +3,7 @@ import jwt from 'jsonwebtoken';
 
 export const verifyJWT = async (req, res, next) => {
   try {
-    const token = req.header('Authorization')?.replace('Bearer ', ''); 
-        || req.cookies?.accessToken
+    const token = req.cookies?.accessToken || req.header('Authorization')?.replace('Bearer ', ''); 
     
 
     if (!token) {
