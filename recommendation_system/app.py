986ef3729d3f2d070aa003e12630d6b4b9775ebf
Himanshu Dhapola <143524162+Himanshu-Dhapola@users.noreply.github.com>
2024-09-06 23:48:05 +0530

    Update app.py

--- recommendation_system/app.py
+++ recommendation_system/app.py
@@ -3,7 +3,7 @@ from recommendation import recommend_products
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["https://himanshu-dhapola-easby.onrender.com"]}})
+CORS(app, resources={r"/*": {"origins": ["https://himanshu-dhapola-easby.vercel.app/"]}})
 
 
 @app.route('/recommend/<product_id>', methods=['GET'])
