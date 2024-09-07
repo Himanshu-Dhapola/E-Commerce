9849b293f92946959d8253ec4dcbeb747fc6229f
Himanshu Dhapola <143524162+Himanshu-Dhapola@users.noreply.github.com>
2024-09-07 10:19:55 +0530

    Update cartApi.js

--- client/src/services/cartApi.js
+++ client/src/services/cartApi.js
@@ -147,12 +147,14 @@ export function updateCartItem(cartData) {
 export function emptyCart() {
   return async (dispatch) => {
     const token = localStorage?.getItem("accessToken");
+    console.log(token);
     try {
       const response = await axios.post(`https://himanshu-dhapola-easby-server.onrender.com/api/v1/cart/empty-cart`,
           {
             headers: { Authorization: `Bearer ${token}` },
             withCredentials: true,
           });
+      console.log(response)
       if (!response.data.success) {
         throw new Error(response.data.message);
       }
