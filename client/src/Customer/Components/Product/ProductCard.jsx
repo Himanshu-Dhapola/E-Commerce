199a47345b5fa5c9eac4b4f94b56dac6dd58a0cf
Himanshu Dhapola <143524162+Himanshu-Dhapola@users.noreply.github.com>
2024-09-07 14:12:45 +0530

    Update ProductCard.jsx

--- client/src/Customer/Components/Product/ProductCard.jsx
+++ client/src/Customer/Components/Product/ProductCard.jsx
@@ -22,7 +22,7 @@ export default function ProductCard({ product }) {
       onClick={() => navigate(`/product/${product?._id}`)}
       className="flex font-Poppins lg:flex-col justify-center items-center border  shadow-lg rounded-2xl bg-white m-2 cursor-pointer transition-all duration-300 hover:scale-105"
     >
-      <div className="w-[220px] h-[220px] flex justify-center items-center">
+      <div className="w-[200px] h-[200px] flex justify-center items-center">
         <img src={product.imageUrl} alt="" className="w-full pt-2 pl-2 pr-2" />
       </div>
       <div className="flex flex-col text-[12px] md:text-base w-full pb-3 pr-3 pl-3">
