f20e8977332b3e969a9c4c5e88bc78fe190755ad
Himanshu Dhapola <143524162+Himanshu-Dhapola@users.noreply.github.com>
2024-09-07 21:23:15 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -17,6 +17,7 @@ import customerProductRouter from './routes/customerProduct.routes.js';
 import adminProductRouter from './routes/adminProduct.routes.js';
 import orderRouter from './routes/customerOrder.routes.js';
 import paymentRouter from './routes/payment.routes.js';
+import orderHistoryRouter from './routes/orderHistory.routes.js'
 
 app.use('/api/v1/customer', customerRouter);
 app.use('/api/v1/products', customerProductRouter);
@@ -25,5 +26,6 @@ app.use('/api/v1/cart', cartRouter);
 app.use('/api/v1/cart_items', cartItemRouter);
 app.use('/api/v1/order', orderRouter);
 app.use('/api/v1/payment', paymentRouter);
+app.use('/api/v1/order_history', orderHistoryRouter);
 
 export { app };
