dad9bd6bd47b8afdcc1ee9614f3949e23c28c6ec
Himanshu Dhapola <143524162+Himanshu-Dhapola@users.noreply.github.com>
2024-09-08 21:13:09 +0530

    Update Signup.jsx

--- client/src/Customer/Components/Signup/Signup.jsx
+++ client/src/Customer/Components/Signup/Signup.jsx
@@ -133,7 +133,7 @@ export default function Signup() {
         </form>
         <button
           onClick={handleRegister}
-          className="bg-color text-white w-[280px] md:w-[350px] px-8 md:py-3 py-2 mt-4 md:mt-8 rounded-lg text-xl font-semibold hover:scale-110 transition-all ease-in duration-200"
+          className="bg-color uppercase text-white w-[280px] md:w-[350px] px-8 md:py-3 py-2 mt-4 md:mt-8 rounded-lg text-xl font-semibold hover:scale-110 transition-all ease-in duration-200"
         >
           Signup
         </button>
