3b3c5a972acd75142f495f1493b04ac633855afb
Himanshu Dhapola <143524162+Himanshu-Dhapola@users.noreply.github.com>
2024-09-11 17:56:45 +0530

    Update app.js
    update CORS

--- server/src/app.js
+++ server/src/app.js
@@ -13,7 +13,7 @@ import orderHistoryRouter from './routes/orderHistory.routes.js';
 
 const app = express();
 
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json({ limit: '64kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
