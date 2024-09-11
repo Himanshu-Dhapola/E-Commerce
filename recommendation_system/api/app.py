0f72d3e94a8adbb31b603a42e368f6474e1d6fb8
Himanshu Dhapola <143524162+Himanshu-Dhapola@users.noreply.github.com>
2024-09-11 23:53:13 +0530

    Update app.py

--- recommendation_system/api/app.py
+++ recommendation_system/api/app.py
@@ -3,12 +3,17 @@ from recommendation import recommend_products
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["https://himanshu-dhapola-easby.onrender.com"]}})
+CORS(app, resources={r"/*": {"origins": ["*"]}})
+
+@app.route("/")
+def hello_world():
+    return "<p>Easby-recommendation-server running</p>"
 
 @app.route('/recommend/<product_id>', methods=['GET'])
 def get_recommendations(product_id): 
     recommended_products = recommend_products(product_id)
     return jsonify(recommended_products.to_dict(orient='records'))
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
