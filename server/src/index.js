9acd613b9b70e857704896c9b1d82967d269e670
Himanshu Dhapola <143524162+Himanshu-Dhapola@users.noreply.github.com>
2024-09-11 16:59:43 +0530

    Update index.js
    removing statement accessing env variables from root directory

--- server/src/index.js
+++ server/src/index.js
@@ -2,8 +2,6 @@ import { connectDB } from "./db/index.js";
 import { app } from "./app.js";
 import dotenv from "dotenv";
 
-dotenv.config({ path: "./.env" });
-
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 3000, () => {
