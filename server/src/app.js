a1442edaff027029ca885e090dfe568645dec768
Himanshu Dhapola <143524162+Himanshu-Dhapola@users.noreply.github.com>
2024-09-11 17:48:54 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -13,14 +13,15 @@ import orderHistoryRouter from './routes/orderHistory.routes.js'
 
 
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Root route is working!');
-});
+
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: '64kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 app.use(cookieParser());
+app.get('/', (req, res) => {
+  res.send('Root route is working!');
+});
 
 app.use('/api/v1/customer', customerRouter);
 app.use('/api/v1/products', customerProductRouter);
