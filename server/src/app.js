c8650f53e67c5eb436914ac49857d92f33bd1860
Himanshu Dhapola <143524162+Himanshu-Dhapola@users.noreply.github.com>
2024-09-11 18:18:35 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,23 +1,23 @@
 import express from 'express';
 import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import { connectDB } from './db/index.js';
-import customerRouter from './routes/customer.routes.js';
-import cartRouter from './routes/cart.routes.js';
-import cartItemRouter from './routes/cartItem.routes.js';
-import customerProductRouter from './routes/customerProduct.routes.js';
-import adminProductRouter from './routes/adminProduct.routes.js';
-import orderRouter from './routes/customerOrder.routes.js';
-import paymentRouter from './routes/payment.routes.js';
-import orderHistoryRouter from './routes/orderHistory.routes.js';
+// import cookieParser from 'cookie-parser';
+// import { connectDB } from './db/index.js';
+// import customerRouter from './routes/customer.routes.js';
+// import cartRouter from './routes/cart.routes.js';
+// import cartItemRouter from './routes/cartItem.routes.js';
+// import customerProductRouter from './routes/customerProduct.routes.js';
+// import adminProductRouter from './routes/adminProduct.routes.js';
+// import orderRouter from './routes/customerOrder.routes.js';
+// import paymentRouter from './routes/payment.routes.js';
+// import orderHistoryRouter from './routes/orderHistory.routes.js';
 
 const app = express();
 
 app.use(cors({ origin: "*", credentials: true }));
-app.use(express.json({ limit: '64kb' }));
-app.use(express.urlencoded({ extended: true, limit: '16kb' }));
-app.use(express.static('public'));
-app.use(cookieParser());
+// app.use(express.json({ limit: '64kb' }));
+// app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+// app.use(express.static('public'));
+// app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('Root route is working!');
